e78527cb73f0344784fe2245a353a74af6701ecf
yuvaltal800 <yuvaltal800@gmail.com>
2025-03-04 21:58:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,36 @@
-%load_ext autoreload
-%autoreload 2
-
-# Download the data from your GitHub repository
-!wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv
-
+#1. Load the dataset:
 import pandas as pd
-df = pd.read_csv('parkinsons.csv') 
+df = pd.read_csv('parkinsons.csv')
 df.head()
 
-x=df[['MDVP:Fo(Hz)','MDVP:Flo(Hz)']]
-y=df['status']
-x.head()
-y.head()
+#2. Select features:
+features = ['MDVP:Flo(Hz)', 'NHR']
+target = ['status']
+x=df[features]
+y=df[target]
 
+#3. Scale the data:
 from sklearn.preprocessing import MinMaxScaler
-scaler = MinMaxScaler(feature_range=(0, 1))
-x_scaled = scaler.fit_transform(x)
+scaler = MinMaxScaler()
+x = scaler.fit_transform(x)
 
+#4. Split the data:
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x_scaled, y, test_size=0.2, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-from sklearn.neighbors import KNeighborsClassifier
-model = KNeighborsClassifier()
+#5. Choose a model:
+from sklearn.svm import SVC
+model = SVC(kernel='linear', C=1.0, random_state=42)
 model.fit(x_train, y_train)
 
-y_perd=model.predict(x_test)
+#6. Test the accuracy:
 from sklearn.metrics import accuracy_score
-accuracy=accuracy_score(y_test,y_perd)
+y_pred = model.predict(x_test)
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Accuracy: {accuracy}')
 
+#Accuracy: 0.8125
 
+#7. Save and upload the model:
 import joblib
-
 joblib.dump(model, 'my_model.joblib')
-
